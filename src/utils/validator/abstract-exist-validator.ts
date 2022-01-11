051f34ea8ca0c30f2ee0ababa0819ba0c5e7cf76
sirkenedy <owoputikehide@gmail.com>
2022-01-11 21:21:57 +0100

    [IMP] Validators and added decorators

---
+++ src/utils/validator/abstract-exist-validator.ts
@@ -0,0 +1,38 @@
+import { ValidationArguments, ValidatorConstraintInterface } from 'class-validator';
+import { Connection, EntitySchema, FindConditions, Not, ObjectType, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+interface ExistValidationArguments<E> extends ValidationArguments {
+  constraints: [
+    ObjectType<E> | EntitySchema<E> | string,
+    string, // column name
+    string // other DTO's property name that will be used to search and skip
+  ];
+}
+
+export abstract class ExistValidator implements ValidatorConstraintInterface {
+  protected constructor(protected connection: Connection) {
+  }
+
+  public async validate<E>(value: string, args: ExistValidationArguments<E>) {
+    let repo = await this.connection.getRepository(args.constraints[0]);
+
+        // @todo: improve this, will be bad if multiple primary keys
+        let primaryKey = await repo.metadata.primaryColumns[0].propertyName;
+
+        let query = {};
+
+        query[args.constraints[1]] = value;
+
+        if(args.constraints[2])
+            query[primaryKey] = Not((args.object as any)[args.constraints[2]]);
+        let count = await repo.count(query);
+        return count > 0;
+  }
+
+  public defaultMessage(args: ValidationArguments) {
+    const [EntityClass] = args.constraints;
+    const entity = EntityClass.name || 'Entity';
+    return `the value supplied for ${this.connection.getRepository(args.constraints[0]).metadata.tableName} is invalid`;
+  }
+}
