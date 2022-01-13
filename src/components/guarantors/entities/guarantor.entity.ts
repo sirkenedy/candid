c495117ed9a2dba5a736162b0b370d9a7c4a38aa
sirkenedy <owoputikehide@gmail.com>
2022-01-13 23:46:32 +0100

    [FIX] updated guarantor and guards module

--- src/components/guarantors/entities/guarantor.entity.ts
+++ src/components/guarantors/entities/guarantor.entity.ts
@@ -6,6 +6,63 @@ export class Guarantors {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
+    @Column()
+    surname: string;
+
+    @Column()
+    otherName: string;
+
+    @Column()
+    phoneNumber: number;
+
+    @Column()
+    image: string;
+
+    @Column()
+    dob: Date;
+
+    @Column()
+    age: number;
+
+    @Column()
+    spouseName: string;
+
+    @Column()
+    residentialAddress: string;
+
+    @Column()
+    religion: string;
+
+    @Column()
+    profession: string;
+
+    @Column()
+    businessName: string;
+
+    @Column()
+    positionHeld: string;
+
+    @Column()
+    officeAddress: string;
+
+    @Column()
+    officePhoneNumber: number;
+
+    @Column()
+    applicantRelationship: string;
+
+    @Column()
+    signature: string;
+
+    @Column()
+    nationality: string;
+
+    @Column({ default: null, type:"datetime"})
+    created_at?:  Date;
+
+    @Column({ default: null, type:"datetime"})
+    updated_at?:  Date;
+
     @ManyToOne(() => Guard, guard => guard.guarantors)
     guard: Guard;
 }
--- src/components/guards/entities/guard.entity.ts
+++ src/components/guards/entities/guard.entity.ts
@@ -1,11 +1,124 @@
+import { Remarks as Remark } from './../../remarks/entities/remark.entity';
 import { Guarantors as Guarantor } from './../../guarantors/entities/guarantor.entity';
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn, OneToOne } from 'typeorm';
 
 @Entity()
 export class Guards {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
+    @Column({default: false})
+    vetStatus: boolean;
+
+    @Column()
+    surname: string;
+
+    @Column()
+    otherName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    image: string;
+
+    @Column()
+    stateOfOrigin: string;
+
+    @Column()
+    lga: string;
+
+    @Column({type:"datetime"})
+    dob: Date;
+
+    @Column({type:"datetime"})
+    age: number;
+
+    @Column()
+    bloodGroup: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    sex: string;
+
+    @Column()
+    religion: string;
+
+    @Column()
+    maritalStatus: string;
+
+    @Column()
+    phoneNo: number;
+
+    @Column()
+    wifeName: string;
+
+    @Column()
+    wifePhoneNumber: number;
+
+    @Column()
+    nextOfKin: string;
+
+    @Column()
+    nextOfKinPhoneNumber: number;
+
+    @Column()
+    eduQualification: string;
+
+    @Column()
+    previousEmployer: string;
+
+    @Column()
+    previousEmployerAddress: string;
+
+    @Column()
+    resignationReason: string;
+
+    @Column()
+    medicalCondition: string;
+
+    @Column()
+    fatherName: string;
+
+    @Column()
+    fatherNameAddress: string;
+
+    @Column()
+    fatherNamePhoneNumber: number;
+
+    @Column()
+    motherName: string;
+
+    @Column()
+    motherNameAddress: string;
+
+    @Column()
+    motherNamePhoneNumber: number;
+
+    @Column()
+    bodyMark: boolean;
+
+    @Column()
+    bodyMarkPart: string;
+
+    @Column()
+    society: string;
+
+    @Column()
+    signature: string;
+
+    @Column({ default: null, type:"datetime"})
+    created_at?:  Date;
+
+    @Column({ default: null, type:"datetime"})
+    updated_at?:  Date;
+
     @OneToMany(() => Guarantor, guarantor => guarantor.guard)
     guarantors?: Guarantor[];
+
+    @OneToOne(() => Remark, remark => remark.guard)
+    @JoinColumn()
+    remark: Remark;
 }
