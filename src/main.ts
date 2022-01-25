19473b5f680a9bd1763e041fdfd83b7f013d6c4e
sirkenedy <owoputikehide@gmail.com>
2022-01-25 22:15:16 +0100

    [FIX] fix cors issues

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.enableCors(
+    {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
+    }
+  );
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
